e5772976473ea5f32bb8f49b5022ee7cf11046ca
Anup Shaw <anupshaw1603@gmail.com>
2024-08-15 00:54:19 +0530

    second commit

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -80,7 +80,7 @@ const index = () => {
             <Wrapper>
                 <Title>Experience</Title>
                 <Desc>
-                    My work experience as a software engineer and working on different companies and projects.
+                    My work experience as a Software Engineer && Trainee Intern that I working on different companies and projects.
                 </Desc>
                 <TimelineSection>
                     <Timeline>
